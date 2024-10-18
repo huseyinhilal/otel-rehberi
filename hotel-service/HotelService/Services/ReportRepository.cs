7a800de42e3c7dd770530699337143c809e202d8
huseyinhilal <huseyinhilall@gmail.com>
2024-10-19 02:10:51 +0300

    feat: Report update with async method 2

--- hotel-service/HotelService/Services/ReportRepository.cs
+++ hotel-service/HotelService/Services/ReportRepository.cs
@@ -17,31 +17,31 @@ namespace HotelService.Services
         public ReportRepository(HotelDbContext context, IServiceScopeFactory scopeFactory)
         {
             _context = context;
-            _scopeFactory = scopeFactory; // Scoped context üretmek için kullanacağız
+            _scopeFactory = scopeFactory; // Scoped context
         }
 
         public async Task<Report> GenerateReportAsync(string location)
         {
-            // İlk olarak raporu "Hazırlanıyor" durumu ile oluşturuyoruz
+            
             var report = new Report
             {
                 Location = location,
                 HotelCount = _context.Hotels.Count(h => h.Location == location),
-                PhoneNumberCount = _context.Hotels.Count(h => h.Location == location), // Basit örnek
+                PhoneNumberCount = _context.Hotels.Count(h => h.Location == location), 
                 Status = "Hazırlanıyor"
             };
 
             _context.Add(report);
             await _context.SaveChangesAsync();
 
-            // Raporu arka planda tamamlamak için bir görev oluşturuyoruz
+            
             Task.Run(async () =>
             {
                 using (var scope = _scopeFactory.CreateScope())
                 {
                     var scopedContext = scope.ServiceProvider.GetRequiredService<HotelDbContext>();
 
-                    await Task.Delay(5000); // Simülasyon: 5 saniye bekliyoruz
+                    await Task.Delay(5000); // Simülasyon: 5 saniye bekle
                     report.Status = "Tamamlandı";
                     scopedContext.Reports.Update(report);
                     await scopedContext.SaveChangesAsync();
