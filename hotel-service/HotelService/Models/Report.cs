8a83f9a0ce9f9eb5258846f50c789c352df55c18
huseyinhilal <huseyinhilall@gmail.com>
2024-10-19 00:53:28 +0300

    perf:Report model update

--- hotel-service/HotelService/Models/Report.cs
+++ hotel-service/HotelService/Models/Report.cs
@@ -1,14 +1,25 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace HotelService.Models
 {
     public class Report
     {
-        public Guid Id { get; set; } = Guid.NewGuid();
-        public string Location { get; set; }
-        public int HotelCount { get; set; }
-        public int ContactCount { get; set; }
-        public DateTime RequestedAt { get; set; } = DateTime.UtcNow;
-        public string Status { get; set; } = "Hazırlanıyor"; // İlk başta "Hazırlanıyor" olacak
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();  
+
+        [Required]  
+        [MaxLength(100)]  
+        public string Location { get; set; } 
+
+        public int HotelCount { get; set; }  
+        public int PhoneNumberCount { get; set; }  
+
+        [Required] 
+        public DateTime RequestedAt { get; set; } = DateTime.Now;  
+
+        [Required]  
+        [MaxLength(20)]  
+        public string Status { get; set; } = "Hazırlanıyor";  
     }
 }
