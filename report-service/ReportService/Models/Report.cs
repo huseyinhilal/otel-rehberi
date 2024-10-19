46cc74e6f5d6ca1b4489b0e4fd382e2d793a8c44
huseyinhilal <huseyinhilall@gmail.com>
2024-10-19 18:27:43 +0300

    feat(report-service):add initial setup for ReportService, required packages

---
+++ report-service/ReportService/Models/Report.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReportService.Models
+{
+    public class Report
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public string Location { get; set; }
+
+        [Required]
+        public int HotelCount { get; set; }
+
+        [Required]
+        public int ContactInfoCount { get; set; }
+
+        public DateTime RequestedAt { get; set; } = DateTime.UtcNow;
+
+        public string Status { get; set; } = "Hazırlanıyor"; // Hazırlanıyor -> Tamamlandı
+    }
+
+}
